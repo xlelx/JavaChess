a5d857d9be722b02a7b1563bcc358a49f83af7bf
Khant Thurein Han <khantthurein@student.unimelb.edu.au>
2020-04-20 03:04:16 +1000

    improved input handling

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Board {
     public final static int BOARD_SIZE = 8;
@@ -40,7 +41,7 @@ public class Board {
         String input = "";
         int cX, cY, toX, toY;
         drawBoard();
-        while (!input.equals("stop")){
+        while (true){
             if(kingInCheck(this.whiteTurn)){
                 if (isCheckmate(this.whiteTurn)){
                     System.out.println((this.whiteTurn ? "Black": "White")+" wins!");
@@ -52,6 +53,18 @@ public class Board {
             System.out.println("Select a piece: ");
             input = scanner.nextLine().toLowerCase();
             int[] parsed = parseInput(input);
+            if (parsed.length == 1){
+                if(input.equals("show")){
+                    drawBoard();
+                    continue;
+                }
+                else if (invalidInput(input)){
+                    continue;
+                }
+                else{
+                    break;
+                }
+            }
             cX = parsed[0];
             cY = parsed[1];
 
@@ -63,7 +76,8 @@ public class Board {
 
             System.out.println("Enter move: ");
             input = scanner.nextLine().toLowerCase();
-            if (input.equals("castle kingside")){
+
+            if (input.equals("castle kingside")) {
                 toX = cX;
                 toY = 6;
             }
@@ -73,6 +87,13 @@ public class Board {
             }
             else{
                 parsed = parseInput(input);
+                if (parsed.length == 1) {
+                    if (invalidInput(input)) {
+                        continue;
+                    } else {
+                        break;
+                    }
+                }
                 toX = parsed[0];
                 toY = parsed[1];
             }
@@ -103,6 +124,20 @@ public class Board {
                 else blackPieces.add(p);
             }
         }
+    }
+    public boolean invalidInput(String input){
+        if(input.equals("stop")){
+            System.out.println("Terminating game...");
+            return false;
+        }
+        else if(input.contains("castle")){
+            System.out.println("Please select your king before castling. Specify either\"kingside\" or \"queenside\"" +
+                    " Example: castle kingside ");
+        }
+        else{
+            System.out.println("Invalid input. Please enter a coordinate in the form of \"A1\"");
+        }
+        return true;
     }
     public static boolean kingInCheck(boolean isWhite){
         ArrayList<Piece> list, kList;
@@ -142,13 +177,21 @@ public class Board {
         }
     }
     public int[] parseInput(String input){
-        char x, y;
-        y = input.charAt(0);
-        x = input.charAt(1);
-        int[] res = new int[2];
-        res[0] = 7 - (x - '1');
-        res[1] = y - 'a';
-        return res;
+        try {
+            char x, y;
+            y = input.charAt(0);
+            x = input.charAt(1);
+
+            if((x < '1' || x > '8') || (y < 'a' || y > 'h')) throw new Exception("error");
+
+            int[] res = new int[2];
+            res[0] = 7 - (x - '1');
+            res[1] = y - 'a';
+            return res;
+        }
+        catch(Exception e){
+            return new int[1];
+        }
     }
     public boolean isCheckmate(boolean isWhite){
         ArrayList<Piece> list = whitePieces;
